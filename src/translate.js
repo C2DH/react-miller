54a41e3543ff3e1341b2259bb22f5280d7e9302b
Daniele Guido <gui.daniele@gmail.com>
2021-03-22 19:10:07 +0100

    Create translate.js

---
+++ src/translate.js
@@ -0,0 +1,52 @@
+export function translateMillerInstance(
+  data,
+  language,
+  defaultLanguage,
+  langs = [],
+) {
+  // Primitive types can't translate them
+  const dataType = typeof data
+  if (
+    data === null ||
+    data === undefined ||
+    ['number', 'string', 'boolean', 'symbol'].includes(dataType)
+  ) {
+    return data
+  }
+  // Iteree over them and translate
+  if (Array.isArray(data)) {
+    return data.map((d) =>
+      translateMillerInstance(d, language, defaultLanguage, langs),
+    )
+  }
+  // Empty object return them
+  const keys = Object.keys(data)
+  if (keys.length === 0) {
+    return data
+  }
+  // CAN TRANSLATE!
+  if (data[language] !== undefined) {
+    return data[language]
+  } else if (defaultLanguage !== null && data[defaultLanguage] !== undefined) {
+    // Use fallback lang!
+    return data[defaultLanguage]
+  } else if (langs && langs.length) {
+    // Ok at this point check if this objects
+    // containts other langs and if return null
+    if (langs.some((lang) => data[lang])) {
+      return null
+    }
+  }
+  // Iteree over values
+  const obj = {}
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i]
+    obj[key] = translateMillerInstance(
+      data[key],
+      language,
+      defaultLanguage,
+      langs,
+    )
+  }
+  return obj
+}
